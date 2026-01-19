aa44c2a10d29e358e6ee67fd85b2003b4de47861
madubashinii <nilakshimadubashini920@gmail.com>
2026-01-19 23:02:49 +0530

    feat: set up app routing and layout structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/customer/Home'
+import Login from './pages/auth/Login'
+import Signup from './pages/auth/Register';
+import Cart from './pages/customer/Cart'
+import MenuPage from './pages/customer/MenuPage';
+import Checkout from './pages/customer/Checkout'
+import Profile from './pages/customer/Profile'
+import AdminDashboard from './pages/admin/Dashboard'
+import ProtectedRoute from './routes/ProtectedRoute'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
+        {/* Protected Customer Routes */}
+        <Route
+          path="/cart"
+          element={
+            // <ProtectedRoute>
+            <Cart />
+            // </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/menu"
+          element={
+            // <ProtectedRoute>
+            <MenuPage />
+            // </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/checkout"
+          element={
+            // <ProtectedRoute>
+            <Checkout />
+            // </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/profile"
+          element={
+            // <ProtectedRoute>
+            <Profile />
+            // </ProtectedRoute>
+          }
+        />
+        {/* Protected Admin Routes */}
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute role="admin">
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
