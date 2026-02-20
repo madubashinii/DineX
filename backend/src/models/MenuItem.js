09fbccb91b89affeb72f500379c5b6a7f94dde0e
madubashinii <nilakshimadubashini920@gmail.com>
2026-02-20 22:00:27 +0530

    add image field to MenuItem model

--- backend/src/models/MenuItem.js
+++ backend/src/models/MenuItem.js
@@ -5,12 +5,11 @@ const menuItemSchema = mongoose.Schema(
         name: { type: String, required: true },
         description: { type: String },
         price: { type: Number, required: true },
-        emoji: { type: String },
+        image: { type: String, required: true },
         category: { type: String, required: true },
     },
     { timestamps: true }
 );
 
 const MenuItem = mongoose.model('MenuItem', menuItemSchema);
-
 export default MenuItem;
